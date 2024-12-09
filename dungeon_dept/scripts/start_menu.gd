ab7fc6a4e592552fb054e0e80a69d7b37e58e193
Joshua Lai <joshualai@MacBook-Pro-100.local>
2024-12-09 15:12:08 -0800

    Start menu

--- dungeon_dept/scripts/start_menu.gd
+++ dungeon_dept/scripts/start_menu.gd
@@ -3,5 +3,4 @@ extends Node2D
 @onready var start_button = $StartButton
 
 func _on_start_button_pressed() -> void:
-	print("YAY YOSEPH")
 	get_tree().change_scene_to_file("res://scenes/dungeon.tscn")
