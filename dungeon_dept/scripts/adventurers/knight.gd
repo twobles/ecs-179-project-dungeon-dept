2d25323e2e8ad2f0ef0e797b1ff585e66fa1e18f
ihhoang <156142939+ihhoang@users.noreply.github.com>
2024-12-13 11:00:36 -0800

    Knight Anims

--- dungeon_dept/scripts/adventurers/knight.gd
+++ dungeon_dept/scripts/adventurers/knight.gd
@@ -1,2 +1,6 @@
 class_name Knight
 extends Adventurer
+
+func _ready() -> void:
+	melee_range = 40
+	super()
--- dungeon_dept/scripts/monsters/monster.gd
+++ dungeon_dept/scripts/monsters/monster.gd
@@ -12,4 +12,15 @@ func _ready() -> void:
 	team = 1
 	facing = Facing.LEFT
 	super()
-	
+
+func face_target() -> void:
+	var cur_pos = global_position
+	var direction = cur_pos.direction_to(enemy.global_position)
+	if direction.x < 0:
+		#print("left")
+		sprite.flip_h = false
+		change_facing(Facing.LEFT)
+	else:
+		#print("right")
+		sprite.flip_h = true
+		change_facing(Facing.RIGHT)
