d6adf3a7f7300e4928e759093c08388d2dd73821
Ezren A <ezrenjoa@gmail.com>
2024-12-12 19:56:59 -0800

    added damage mechanics to golem and slime scenes

--- dungeon_dept/scripts/monsters/golem.gd
+++ dungeon_dept/scripts/monsters/golem.gd
@@ -2,6 +2,11 @@ class_name Golem
 extends Monster
 
 @onready var golem_sfx: AudioStreamPlayer = AudioStreamPlayer.new()
+@onready var damage_taken_indicator: Label = $DamageTaken
+var _death: bool = false
+var dam_ind_delay: int = 0
+var prev_damage_taken: int = 0
+
 
 func _init() -> void:
 	cost = 40
@@ -13,8 +18,24 @@ func _ready() -> void:
 	golem_sfx.stream = preload("res://sounds/SFX/golem_sfx.wav")
 	golem_sfx.play()
 	melee_range = 60
+	damage_taken_indicator.text = " "
 	super()
 
 
 func _process(_delta: float) -> void:
-	pass
+	if dam_ind_delay > 0:
+		damage_taken_indicator.text = str(prev_damage_taken)
+		dam_ind_delay -= 1
+	else:
+		damage_taken_indicator.text = " "
+
+
+func take_damage(d: int) -> void:
+	print("golem hit!")
+	health -= d
+	prev_damage_taken = d
+	dam_ind_delay = 30
+	if 0 >= health:
+		_death = true
+		print("golem DEAD")
+		# FIXME: do stuff like remove when dies. make death funct?
--- dungeon_dept/scripts/monsters/slime.gd
+++ dungeon_dept/scripts/monsters/slime.gd
@@ -2,6 +2,10 @@ class_name Slime
 extends Monster
 
 @onready var slime_sfx: AudioStreamPlayer = AudioStreamPlayer.new()
+@onready var damage_taken_indicator: Label = $DamageTaken
+var _death: bool = false
+var dam_ind_delay: int = 0
+var prev_damage_taken: int = 0
 
 
 func _init() -> void:
@@ -14,8 +18,24 @@ func _ready() -> void:
 	slime_sfx.stream = preload("res://sounds/SFX/slime_sfx.wav")
 	slime_sfx.play()
 	melee_range = 90
+	damage_taken_indicator.text = " "
 	super()
 	
 	
 func _process(_delta: float) -> void:
-	pass
+	if dam_ind_delay > 0:
+		damage_taken_indicator.text = str(prev_damage_taken)
+		dam_ind_delay -= 1
+	else:
+		damage_taken_indicator.text = " "
+
+
+func take_damage(d: int) -> void:
+	print("slime hit!")
+	health -= d
+	prev_damage_taken = d
+	dam_ind_delay = 30
+	if 0 >= health:
+		_death = true
+		print("slime DEAD")
+		# FIXME: do stuff like remove when dies. make death funct?
